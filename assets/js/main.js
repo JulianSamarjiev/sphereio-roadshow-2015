5062905ecc66673b25ac5402be751dd11fb447af
Julian Samarjiev <juliansamarjiev@gmail.com>
2015-04-08 17:19:46 +0200

    brushing up v1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,20 @@
+// Add .active on click 
+
 var selector = '.day-toggle .timeline-btn';
 
 $(selector).on('click', function(){
     $(selector).removeClass('active');
     $(this).addClass('active');
 });
+
+// Close hamburger menu on click
+
+$(document).ready(function () {
+  $(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
+});
+
+// Close hamburger when clicking outside of it
+
 
